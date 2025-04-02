04f8f5c480e477ad338821fcd58b403230bf18b1
Walancy.S <95140989+Walancy@users.noreply.github.com>
2025-04-02 19:26:52 -0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <section className="footer">
       <p>
-        © 2023 Walancy Heleonai Ferreira dos Santos.{" "}
+        © 2025 Walancy Heleonai Ferreira dos Santos.{" "}
         <span>Todos os direitos reservados.</span>
       </p>
     </section>
